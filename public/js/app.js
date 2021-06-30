9284025ababc1dc5fbe5d1160a9cd4dfe121228f
emirsagit <37173514+emirsagit@users.noreply.github.com>
2021-06-30 15:28:03 +0300

    Create navigation menu

--- public/js/app.js
+++ public/js/app.js
@@ -1,7 +1,31 @@
+/**
+ * Add css border to nav element for displaying current page
+ */
 const path = window.location.pathname;
-console.log(path);
 if (path === '/') {
   document.getElementById('home').classList.add('nav__link--border');
 } else if (path === '/about.html') {
   document.getElementById('about').classList.add('nav__link--border');
+}
+
+/**
+ * Mobile menu navigation bar display and hide
+ */
+const toggleElements = ['hamburger', 'close-button', 'home', 'about'];
+
+const nav = document.getElementById('nav');
+function toggle() {
+  if (!nav.classList.contains('nav--mobile-hidden')) {
+    nav.style.animation = 'close 1s ease-in-out';
+    setTimeout(() => {
+      nav.classList.toggle('nav--mobile-hidden');
+    }, 1000);
+  } else {
+    nav.style.animation = 'open 1s ease-in-out';
+    nav.classList.toggle('nav--mobile-hidden');
+  }
+}
+
+for (let i = 0; i < toggleElements.length; i = +1) {
+  document.getElementById(toggleElements[i]).addEventListener('click', toggle);
 }
